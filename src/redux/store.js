2a005e3b5f56a22958fdbd1a6a7f95cbd2c8bfec
sheriallis <shrdson@gmail.com>
2020-03-10 11:48:49 +0100

    Add redux-persist to persist the cart reducer across sessions

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,17 @@
 import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
 // import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
 
 // const middlewares = [logger];
 
-const store = createStore(
+export const store = createStore(
   rootReducer,
   // applyMiddleware(...middlewares),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-export default store;
+export const persistor = persistStore(store);
+
+export default { store, persistor };
